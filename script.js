6fc6ba049ac0601e66eb41897c8fb0e4b1945f40
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-10 17:15:09 +0545

    final commit

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 function MyChoiceIs(userChoice) {
-    document.querySelector('.computer-choice').className = 'computer-choice stop-spinning'
-    const randomNumber = Math.ceil(Math.random() * 2);
-    const computerChoice = ["rock", "paper", "scissors"][randomNumber];
-    document.querySelector(".computer-choice ." + computerChoice).style.display = "block";
+  document.querySelector(".computer-choice").className =
+    "computer-choice stop-spinning";
+  const randomNumber = Math.ceil(Math.random() * 2);
+  const computerChoice = ["rock", "paper", "scissors"][randomNumber];
+  document.querySelector(".computer-choice ." + computerChoice).style.display =
+    "block";
 
-    const draw = userChoice === computerChoice;
-    if (draw) {
-        document.querySelector('.result').innerText = 'Draw!';
+  const draw = userChoice === computerChoice;
+  if (draw) {
+    document.querySelector(".result").innerText = "Draw!";
+  } else {
+    const userWon =
+      (userChoice === "rock" && computerChoice == "scissors") ||
+      (userChoice === "paper" && computerChoice == "rock") ||
+      (userChoice === "scissors" && computerChoice === "paper");
+    if (userWon) {
+      document.querySelector(".result").innerText = "You won!";
     } else {
-        const userWon = (userChoice === 'rock' && computerChoice == 'scissors') || (userChoice === 'paper' && computerChoice == 'rock') || (userChoice === 'scissors' && computerChoice === 'paper');
-        if (userWon) {
-            document.querySelector('.result').innerText = 'You won!';
-        } else {
-            document.querySelector('.result').innerText = 'You lost!';
-        }
+      document.querySelector(".result").innerText = "You lost!";
     }
-    setTimeout(function () {
-        window.location.reload();
-    }, 2000)
   }
+  setTimeout(function () {
+    window.location.reload();
+  }, 2000);
+}
