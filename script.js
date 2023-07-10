962bb4747a420aee6f09cc988d06b03c6ea69fbf
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-10 16:49:12 +0545

    js

---
+++ script.js
@@ -0,0 +1,5 @@
+function MyChoiceIs(userChoice) {
+    const randomNumber = Math.ceil(Math.random() * 2);
+    const computerChoice = ["rock", "paper", "scissors"][randomNumber];
+    document.querySelector(".computer-choice ." + computerChoice).style.display = "block";
+  }
