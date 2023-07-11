899992af4c8ac795971051276686fbf96b6a993b
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-11 09:43:21 +0545

    final commit

--- script.js
+++ script.js
@@ -3,16 +3,15 @@ function MyChoiceIs(userChoice) {
     "computer-choice stop-spinning";
   const randomNumber = Math.ceil(Math.random() * 2);
   const computerChoice = ["rock", "paper", "scissors"][randomNumber];
-  document.querySelector(".computer-choice ." + computerChoice).style.display =
-    "block";
+  document.querySelector(".computer-choice ." + computerChoice).style.display = "block";
 
   const draw = userChoice === computerChoice;
   if (draw) {
     document.querySelector(".result").innerText = "Draw!";
   } else {
     const userWon =
-      (userChoice === "rock" && computerChoice == "scissors") ||
-      (userChoice === "paper" && computerChoice == "rock") ||
+      (userChoice === "rock" && computerChoice === "scissors") ||
+      (userChoice === "paper" && computerChoice === "rock") ||
       (userChoice === "scissors" && computerChoice === "paper");
     if (userWon) {
       document.querySelector(".result").innerText = "You won!";
